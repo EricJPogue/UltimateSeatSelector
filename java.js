1624b49bca33305d561d9189237ed94b95bd38e3
JamesOdame <jamesaodame@lewisu.edu>
2022-02-09 22:20:20 -0600

    Created two new files Index & Java

---
+++ java.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box").innerHTML = firstName + lastName;
+
+        }
+function myFunction1() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box1").innerHTML = firstName + lastName;
+
+    }
+function myFunction2() {
+    var x;
+        let firstName = prompt("First Name: ")
+        let lastName = prompt("Last Name: ")
+        console.log(firstName);
+        console.log(lastName);
+        document.getElementById("Box2").innerHTML = firstName + lastName;
+
+        }
